b3d507cb310ac2a590732d1ea4a05e6eed870d8f
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-03-27 00:05:08 -0700

    Database File created

--- engine/database.py
+++ engine/database.py
@@ -8,9 +8,44 @@ class Database:
 
     def open(self):
         self._connection = sqlite3.connect(self._path)
-        cursor = self._connection.cursor()
         cursor = self._connection.execute(""" PRAGMA foreign_keys = ON; """)
         cursor.close()
 
     def close(self):
         self._connection.close()
+
+    def createDatabaseFile(self):
+        """ This is only used once to create the database"""
+        cursor = self._connection.cursor()
+        cursor.execute("""
+            CREATE TABLE guest (
+                guestID INTEGER PRIMARY KEY ,
+                firstName text NOT NULL ,
+                lastName text NOT NULL ,
+                email text NOT NULL ,
+                phone text NOT NULL
+            ); 
+        """)
+        cursor.execute("""
+            CREATE TABLE room (
+                roomNumber INTEGER PRIMARY KEY ,
+                guestID INTEGER NOT NULL,
+                type text NOT NULL,
+                FOREIGN KEY(guestID)  REFERENCES guest(guestID)   
+            ); 
+        """)
+        cursor.execute("""
+            CREATE TABLE reservation (
+                roomNumber INTEGER NOT NULL ,
+                guestID INTEGER NOT NULL,
+                startDate date NOT NULL,  
+                EndDate date NOT NULL , 
+                FOREIGN KEY (roomNumber) REFERENCES room(roomNumber),
+                FOREIGN KEY (guestID) REFERENCES guest(guestID)
+            );
+        """)
+
+if __name__ == "__main__":
+    d = Database("databaseFile.db")
+    d.open()
+    d.close()
